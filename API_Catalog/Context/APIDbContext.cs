3d66044626fca57f790eebdb258f44b7fceeb7dd
Leonardo <lleonardo1555@gmail.com>
2023-04-17 22:25:56 -0300

    Adicionar arquivos de projeto.

---
+++ API_Catalog/Context/APIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API_Catalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Catalog.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) 
+            : base(options) => Database.EnsureCreated();
+
+       
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ API_Catalog/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace API_Catalog.Models;//novo recurso do .net6 não é preciso as chaves {} no namespace
+
+public class Categoria
+{
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+}
