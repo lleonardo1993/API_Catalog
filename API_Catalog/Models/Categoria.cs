7faeea6a6290fe4a57eeef05367667eb543c5ebe
Leonardo <lleonardo1555@gmail.com>
2023-04-18 22:25:40 -0300

    Criação das Migrations.

--- API_Catalog/Models/Categoria.cs
+++ API_Catalog/Models/Categoria.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_Catalog.Models;//novo recurso do .net6 não é preciso as chaves {} no namespace
 
+[Table("Categoria")]//table e key, redundantes póis ja foram mapeados no context
 public class Categoria
-{
-
+{    
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
-
+    [Key]
     public int CategoriaId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
--- API_Catalog/Models/Produto.cs
+++ API_Catalog/Models/Produto.cs
@@ -1,11 +1,26 @@
-﻿namespace API_Catalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Catalog.Models
 {
+    [Table("Produto")]
     public class Produto
     {
 
+        [Key]
         public int ProdutoId { get; set; }
+        
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+        
+        [Required]
+        [StringLength(300)]
         public string? Descricao { get; set; }
+        
+        [Required]
+        [Column(TypeName ="decimal(10,2)")]
+        
         public decimal Preco { get; set; }
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
