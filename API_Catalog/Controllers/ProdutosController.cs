50909c3769e468b766b492d3062c1a9e70562968
Leonardo <lleonardo1555@gmail.com>
2023-04-27 23:09:25 -0300

    Injeção de dependencia da controller, e criação do metodo get.

---
+++ API_Catalog/Controllers/ProdutosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Catalog.Data;
+using API_Catalog.Models;
+
+namespace API_Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly APIDbContext _context;
+
+        public ProdutosController(APIDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+
+    }
+}
